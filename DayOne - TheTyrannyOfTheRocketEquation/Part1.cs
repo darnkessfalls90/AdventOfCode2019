b5d3b68ebd7207306a1788e0f1a13b567d673f17
Josh Sharp <jsharp9009@live.com>
2019-12-01 22:25:45 -0600

    Part 2 and structure changes

---
+++ DayOne - TheTyrannyOfTheRocketEquation/Part1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheTyrannyOfTheRocketEquation
+{
+    class Part1
+    {
+        public static void Execute(List<int> MyModules, bool interactive)
+        {
+            long fuelForCurrentModules = 0;
+            MyModules.ForEach(m => fuelForCurrentModules += CalculateFuelRequirement(m));
+            Console.WriteLine("Current Module Weight: " + fuelForCurrentModules); 
+
+            while (interactive)
+            {
+                Console.WriteLine("Enter Mass: ");
+                var massString = Console.ReadLine();
+                if (massString == "x")
+                {
+                    break;
+                }
+                if (!int.TryParse(massString, out int mass))
+                {
+                    Console.WriteLine("Invalid Input. Please enter a valid integer.");
+                    continue;
+                }
+
+                var fuel = CalculateFuelRequirement(mass);
+                Console.WriteLine("Fuel Requirement: " + fuel);
+            }
+        }
+
+        public static int CalculateFuelRequirement(int mass)
+        {
+            return Convert.ToInt32(Math.Floor(mass / 3.0)) - 2;
+        }
+    }
+}
---
+++ DayOne - TheTyrannyOfTheRocketEquation/Part2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheTyrannyOfTheRocketEquation
+{
+    class Part2
+    {
+        public static void Execute(List<int> MyModules, bool interactive)
+        {
+            long fuelForCurrentModules = 0;
+            MyModules.ForEach(m => fuelForCurrentModules += CalculateFuelRequirement(m));
+            Console.WriteLine("Current Module Weight: " + fuelForCurrentModules); 
+
+            while (interactive)
+            {
+                Console.WriteLine("Enter Mass: ");
+                var massString = Console.ReadLine();
+                if (massString == "x")
+                {
+                    break;
+                }
+                if (!int.TryParse(massString, out int mass))
+                {
+                    Console.WriteLine("Invalid Input. Please enter a valid integer.");
+                    continue;
+                }
+
+                var fuel = CalculateFuelRequirement(mass);
+                Console.WriteLine("Fuel Requirement: " + fuel);
+            }
+        }
+
+        public static int CalculateFuelRequirement(int mass)
+        {
+            var fuelRequirement = Convert.ToInt32(Math.Floor(mass / 3.0)) - 2;
+
+            if (fuelRequirement > 0)
+            {
+                var fuelForFuel = CalculateFuelRequirement(fuelRequirement);
+                if(fuelForFuel > 0)
+                    fuelRequirement += fuelForFuel;
+            }
+                
+            return fuelRequirement;
+        }
+    }
+}
