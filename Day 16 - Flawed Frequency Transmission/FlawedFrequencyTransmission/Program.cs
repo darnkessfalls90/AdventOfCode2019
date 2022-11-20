db4e4bdf349a58561c165626d334071bef994f8e
jsharp9009 <jsharp9009@live.com>
2022-11-20 13:51:31 -0600

    Day 16 Done! and it works fast!

--- Day 16 - Flawed Frequency Transmission/FlawedFrequencyTransmission/Program.cs
+++ Day 16 - Flawed Frequency Transmission/FlawedFrequencyTransmission/Program.cs
@@ -2,7 +2,7 @@
 {
     public class FlawedFrequencyTransmission
     {
-        static int[] BasePattern = new int[] { 0, 1, 0, -1 };
+        //static int[] BasePattern = new int[] { 0, 1, 0, -1 };
 
         public static void Main(string[] args)
         {
@@ -22,15 +22,16 @@
                 signalReceivedList.AddRange(signalReceivedListOrig);
             }
 
-            var signalReceived = signalReceivedList.ToArray();
+            var signalReceived = signalReceivedList.Skip(offsetInt).ToArray();
 
             for (int i = 0; i < 100; i++)
             {
+                //Console.WriteLine(i);
                 signalReceived = Phase(signalReceived);
             }
 
             
-            var message = signalReceived.Skip(offsetInt+1).Take(8).ToArray();
+            var message = signalReceived.Take(8).ToArray();
 
             for (int i = 0; i < message.Length; i++)
             {
@@ -43,43 +44,48 @@
         public static int[] Phase(int[] input)
         {
             var result = new int[input.Length];
-            for (int i = 0; i < input.Length; i++)
+            var rolling = 0;
+            for (int i = input.Length - 1; i >= 0 ; i--)
             {
-                var pattern = CreatePattern(i + 1, input.Length);
-                var newVal = 0;
-                for (int n = 0; n < input.Length; n++)
-                {
-                    newVal += input[n] * pattern[n];
-                }
+                //var pattern = CreatePattern(i + 1, input.Length);
+                //var newVal = 0;
+                //var partsthatMatter = pattern.Where(a => a != 0)
+                //    .Select((p, index) => new KeyValuePair<int, int>(p, index));
+                //var parity = 1;
+                //for (int n = i; n < input.Length; n += ((i+1)*2))
+                //{
+                //    newVal += (input.Skip(n).Take((i+1)).Sum() * parity);
+                //    //parity *= -1;
+                //}
 
-                newVal = int.Parse(newVal.ToString().Last().ToString());
-                result[i] = newVal;
+                rolling += input[i];
+                result[i] = rolling % 10;
             }
             return result;
         }
 
-        static Dictionary<int, int[]>  existingPatterns = new Dictionary<int, int[]>();
-        public static int[] CreatePattern(int place, int length)
-        {
-            if (existingPatterns.ContainsKey(place)) return existingPatterns[place];
+        //static Dictionary<int, int[]>  existingPatterns = new Dictionary<int, int[]>();
+        //public static int[] CreatePattern(int place, int length)
+        //{
+        //    if (existingPatterns.ContainsKey(place)) return existingPatterns[place];
 
-            List<int> pattern = new List<int>();
-            for(int i = 0; i < BasePattern.Length; i++)
-            {
-                for(int n = 0; n < place; n++)
-                {
-                    pattern.Add(BasePattern[i]);
-                }
-            }
+        //    List<int> pattern = new List<int>();
+        //    for(int i = 0; i < BasePattern.Length; i++)
+        //    {
+        //        for(int n = 0; n < place; n++)
+        //        {
+        //            pattern.Add(BasePattern[i]);
+        //        }
+        //    }
 
-            while(pattern.Count < length + 1)
-            {
-                pattern.AddRange(pattern);
-            }
+        //    while(pattern.Count < length + 1)
+        //    {
+        //        pattern.AddRange(pattern);
+        //    }
 
-            var patternArr = pattern.Skip(1).Take(length).ToArray();
-            existingPatterns.Add(place, patternArr);
-            return patternArr;
-        }
+        //    var patternArr = pattern.Skip(1).Take(length).ToArray();
+        //    existingPatterns.Add(place, patternArr);
+        //    return patternArr;
+        //}
     }
 }
