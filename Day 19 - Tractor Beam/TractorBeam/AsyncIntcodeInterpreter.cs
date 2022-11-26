aaa089d3a67f35168aaea71665d5181c3c193430
jsharp9009 <jsharp9009@live.com>
2022-11-26 16:36:23 -0600

    Day 17 switching machines

---
+++ Day 19 - Tractor Beam/TractorBeam/AsyncIntcodeInterpreter.cs
@@ -0,0 +1,81 @@
+﻿using Intcode;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TractorBeam
+{
+    public class AsyncIntcodeInterpreter : IDisposable
+    {
+        BackgroundWorker bg;
+        Queue<long> output = new Queue<long>();
+        Queue<int> input = new Queue<int>();
+
+        public void Begin(long[] program)
+        {
+            if (bg != null) bg.Dispose();
+            bg = new BackgroundWorker();
+            bg.DoWork += BackgroundWorker_DoWork;
+
+            var computer = new IntcodeInterpreter();
+            computer.TakeInput += Computer_TakeInput;
+            computer.HandleOutput += Computer_HandleOutput;
+
+            bg.RunWorkerAsync(new { computer = computer, program = program });
+        }
+
+        public void Dispose()
+        {
+            if (bg != null)
+                bg.Dispose();
+        }
+
+        public void GiveInput(int input)
+        {
+            this.input.Enqueue(input);
+        }
+
+        public long[] RecieveOutput(int legnth)
+        {
+            while (output.Count < legnth)
+                Thread.Sleep(100);
+            return output.DequeueMany(legnth).ToArray();
+        }
+
+        private void BackgroundWorker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            
+            var program = ((dynamic)e.Argument).program as long[];
+            ((dynamic)e.Argument).computer.Interpret(ref program);
+        }
+
+        private void Computer_HandleOutput(long output, ref bool pause)
+        {
+            this.output.Enqueue(output);
+        }
+
+        private int? Computer_TakeInput()
+        {
+            while (this.input.Count == 0)
+                Thread.Sleep(100);
+            return this.input.Dequeue();
+        }
+    }
+
+    public static class QueueExtender
+    {
+        public static IEnumerable<T> DequeueMany<T>(this Queue<T> queue, int length)
+        {
+            var list = new List<T>();
+            for(int i = 0; i < length; i++)
+            {
+                list.Add(queue.Dequeue());
+            }
+            return list.AsEnumerable<T>();
+        }
+    }
+}
---
+++ Day 19 - Tractor Beam/TractorBeam/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Intcode;
+
+namespace TractorBeam
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = IntcodeInterpreter.ReadInput("input.txt");
+            var map = MapTractorBeams(input, out int total);
+            DrawMap(map);
+            Console.WriteLine("Total Beams: " + total);
+            Console.ReadLine();
+        }
+
+        public static long[,] MapTractorBeams(long[] input, out int total)
+        {
+            total = 0;
+            var result = new long[50,50];
+            var computer = new AsyncIntcodeInterpreter();
+            var minx = 0;
+            for (int y = 0; y < 50; y++)
+            {
+                bool foundOne = false;
+                for(int x = minx; x < 50; x++)
+                {
+                    computer.Begin(input);
+
+                    computer.GiveInput(x);
+                    computer.GiveInput(y);
+                    var space = computer.RecieveOutput(1).First();
+                    if (space == 1)
+                    {
+                        if (!foundOne)
+                            minx = x;
+                        result[x, y] = 1;
+                        total++;
+                        foundOne = true;
+                    }
+                    else
+                    {
+                        if (foundOne) break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static void DrawMap(long[,] map)
+        {
+            for (int y = 0; y < 50; y++)
+            {
+                Console.Write(y < 10 ? " " + y : y);
+                for (int x = 0; x < 50; x++)
+                {
+                    Console.Write(map[x, y] == 1 ? (x < 10 ? " " + x : x) : " .");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
