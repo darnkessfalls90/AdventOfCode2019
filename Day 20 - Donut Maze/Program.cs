c8a279dbdcf573c4314c6759d36ff62e9f9b332e
jsharp <joshua.sharp@communitybrands.com>
2023-10-13 13:08:46 -0500

    Day 20 Finished!

--- Day 20 - Donut Maze/Program.cs
+++ Day 20 - Donut Maze/Program.cs
@@ -14,16 +14,18 @@ namespace DonutMaze
         public static void Main(string[] args)
         {
             var textInput = File.ReadAllLines("input.txt");
-            var map = MapInput(textInput, out Dictionary<Point, string> doors);
+            var map = MapInput(textInput, out Dictionary<Vector2, string> doors);
             var start = doors.First(v => v.Value.Equals("AA"));
-            var steps = WalkMapPart1(start.Key, map, doors);
-            Console.WriteLine("Shortest Steps Part 1: " + steps);
+            //var steps = WalkMapPart1(start.Key, map, doors);
+            //Console.WriteLine("Shortest Steps Part 1: " + steps);
+            var steps = WalkMapPart2(new Vector3(start.Key, 0), map, doors);
+            Console.WriteLine("Shortest Steps Part 2: " + steps);
         }
 
-        public static char[,] MapInput(string[] input, out Dictionary<Point, string> doors)
+        public static char[,] MapInput(string[] input, out Dictionary<Vector2, string> doors)
         {
             var ret = new char[input.First().Length, input.Length];
-            doors = new Dictionary<Point, string >();
+            doors = new Dictionary<Vector2, string >();
             for (int y = 0; y < input.Length - 1; y++)
             {
                 for (int x = 0; x < input[y].Length - 1; x++)
@@ -33,22 +35,22 @@ namespace DonutMaze
 
                     if (char.IsLetter(character))
                     {
-                        if(IsDoorEntrance(input, new Point(x, y))){
+                        if(IsDoorEntrance(input, new Vector2(x, y))){
                             if (y < input.Length - 1 && char.IsLetter(input[y + 1][x]))
                             {
-                                doors.Add(new Point(x, y), string.Concat(input[y + 1][x], character));
+                                doors.Add(new Vector2(x, y), string.Concat(input[y + 1][x], character));
                             }
                             else if(y > 0 && char.IsLetter(input[y - 1][x]))
                             {
-                                doors.Add(new Point(x, y), string.Concat(character, input[y - 1][x]));
+                                doors.Add(new Vector2(x, y), string.Concat(character, input[y - 1][x]));
                             }
                             else if (x < input[y].Length - 1 && char.IsLetter(input[y][x + 1]))
                             {
-                                doors.Add(new Point(x, y), string.Concat(input[y][x+1], character));
+                                doors.Add(new Vector2(x, y), string.Concat(input[y][x+1], character));
                             }
                             else if (x > 0 && char.IsLetter(input[y][x - 1]))
                             {
-                                doors.Add(new Point(x, y), string.Concat(character, input[y][x - 1] ));
+                                doors.Add(new Vector2(x, y), string.Concat(character, input[y][x - 1] ));
                             }
                         }
                     }
@@ -57,10 +59,10 @@ namespace DonutMaze
             return ret;
         }
         
-        public static int WalkMapPart1(Point startingPoint, char[,] map, Dictionary<Point, string> doors)
+        public static int WalkMapPart1(Vector2 startingPoint, char[,] map, Dictionary<Vector2, string> doors)
         {
-            var que = new Queue<Point>();
-            var distance = new Dictionary<Point, int>();
+            var que = new Queue<Vector2>();
+            var distance = new Dictionary<Vector2, int>();
             que.Enqueue(startingPoint);
             distance.Add(startingPoint, -1);
 
@@ -71,15 +73,15 @@ namespace DonutMaze
                 foreach(var point in GetSurrounding(currentPoint))
                 {
                     if (distance.ContainsKey(point)) continue;
-                    if (map[point.X, point.Y].Equals('.'))
+                    if (map[(int)point.X, (int)point.Y].Equals('.'))
                     {
                         que.Enqueue(point);
                         distance.Add(point, distance[currentPoint] + 1);
                     }
-                    if(char.IsLetter(map[point.X, point.Y]))
+                    if(char.IsLetter(map[(int)point.X, (int)point.Y]))
                     {
                         var door = doors.FirstOrDefault(c => c.Key == point);
-                        if (door.Equals(default(KeyValuePair<Point, string>))) continue;
+                        if (door.Equals(default(KeyValuePair<Vector2, string>))) continue;
 
                         if(door.Value == "ZZ")
                         {
@@ -87,11 +89,11 @@ namespace DonutMaze
                         }
 
                         var otherDoor = doors.First(c => c.Value == door.Value && !c.Equals(door));
-                        if (otherDoor.Equals(default(KeyValuePair<string, Point>))) throw new Exception("Could not find other door");
+                        if (otherDoor.Equals(default(KeyValuePair<string, Vector2>))) throw new Exception("Could not find other door");
 
                         foreach(var newPoint in GetSurrounding(otherDoor.Key))
                         {
-                            if (map[newPoint.X, newPoint.Y].Equals('.'))
+                            if (map[(int)newPoint.X, (int)newPoint.Y].Equals('.'))
                             {
                                 distance.Add(door.Key, distance[currentPoint] + 1);
                                 distance.Add(otherDoor.Key, distance[currentPoint] + 1);
@@ -105,23 +107,96 @@ namespace DonutMaze
             return 0;            
         }
 
-        static List<Point> transforms = new List<Point>
+        public static int WalkMapPart2(Vector3 startingPoint, char[,] map, Dictionary<Vector2, string> doors)
         {
-            new Point(0, 1),
-            new Point(1, 0),
-            new Point(0, -1),
-            new Point(-1, 0),
+            var que = new Queue<Vector3>();
+            var distance = new Dictionary<Vector3, int>();
+            que.Enqueue(startingPoint);
+            distance.Add(startingPoint, -1);
+
+            while (que.Count > 0)
+            {
+                var currentPoint = que.Dequeue();
+
+                foreach (var point in GetSurrounding(currentPoint))
+                {
+                    if (distance.ContainsKey(point)) continue;
+                    if (map[(int)point.X, (int)point.Y].Equals('.'))
+                    {
+                        que.Enqueue(point);
+                        distance.Add(point, distance[currentPoint] + 1);
+                    }
+                    if (char.IsLetter(map[(int)point.X, (int)point.Y]))
+                    {
+                        var door = doors.FirstOrDefault(c => c.Key.X == point.X && c.Key.Y == point.Y);
+                        if (door.Equals(default(KeyValuePair<Vector2, string>))) continue;
+
+                        if (door.Value == "ZZ")
+                        {
+                            if (currentPoint.Z == 0)
+                                return distance[currentPoint];
+                            else
+                                continue;
+                        }
+                        if (door.Value == "AA")
+                        {
+                            continue;
+                        }
+
+                        if (currentPoint.Z == 0 && IsDoorOutside(map, door.Key))
+                        {
+                            continue;
+                        }
+
+                        var otherDoor = doors.First(c => c.Value == door.Value && !c.Equals(door));
+                        if (otherDoor.Equals(default(KeyValuePair<string, Vector2>))) throw new Exception("Could not find other door");
+
+                        foreach (var newPoint in GetSurrounding(otherDoor.Key))
+                        {
+                            if (map[(int)newPoint.X, (int)newPoint.Y].Equals('.'))
+                            {
+                                var parity = IsDoorOutside(map, door.Key) ? -1 : 1;
+                                var newVector = new Vector3(newPoint, currentPoint.Z + parity);
+
+                                if (distance.ContainsKey(newVector)) continue;
+
+                                distance.Add(new Vector3(door.Key, currentPoint.Z), distance[currentPoint] + 1);
+                                distance.Add(new Vector3(otherDoor.Key, currentPoint.Z + parity), distance[currentPoint] + 1);
+                                que.Enqueue(newVector);
+                                distance.Add(newVector, distance[currentPoint] + 1);
+                            }
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+
+        static List<Vector2> transforms = new List<Vector2>
+        {
+            new Vector2(0, 1),
+            new Vector2(1, 0),
+            new Vector2(0, -1),
+            new Vector2(-1, 0),
         };
 
-        public static IEnumerable<Point> GetSurrounding(Point point)
+        public static IEnumerable<Vector2> GetSurrounding(Vector2 point)
         {
-            foreach(Point trans in transforms)
+            foreach(Vector2 trans in transforms)
             {
-                yield return new Point(point.X + trans.X, point.Y + trans.Y);
+                yield return new Vector2(point.X + trans.X, point.Y + trans.Y);
             }
         }
 
-        public static bool IsDoorEntrance(string[] input, Point point)
+        public static IEnumerable<Vector3> GetSurrounding(Vector3 point)
+        {
+            foreach (Vector2 trans in transforms)
+            {
+                yield return new Vector3(point.X + trans.X, point.Y + trans.Y, point.Z);
+            }
+        }
+
+        public static bool IsDoorEntrance(string[] input, Vector2 point)
         {
             bool nextToLetter = false;
             bool nextToPath = false;
@@ -130,10 +205,18 @@ namespace DonutMaze
                 if (nexTo.Y < 0 || nexTo.Y > input.Length - 1) continue;
                 if (nexTo.X < 0 || nexTo.X > input[0].Length - 1) continue;
 
-                if (input[nexTo.Y][nexTo.X] == '.') nextToPath = true;
-                if(char.IsLetter(input[nexTo.Y][nexTo.X])) nextToLetter = true;
+                if (input[(int)nexTo.Y][(int)nexTo.X] == '.') nextToPath = true;
+                if(char.IsLetter(input[(int)nexTo.Y][(int)nexTo.X])) nextToLetter = true;
             }
             return nextToPath && nextToLetter;
         }
+
+        public static bool IsDoorOutside(char[,] map, Vector2 door)
+        {
+            if (door.X < 2 || door.X > map.GetLength(0) - 3) return true;
+            if (door.Y < 2 || door.Y > map.GetLength(1) - 3) return true;
+            return false;
+        }
+
     }
 }
